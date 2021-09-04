56f1369c71a9ab8ba9499e01b4d00c50f335ecb0
root <eyuelmulugeta60@gmail.com>
2021-09-04 08:45:16 -0700

    c programms

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,17 +4,16 @@
  *
  * Return: always 0.
  */
-int main()
+int main(void)
 {
 int i = 0;
 char c;
-char s[] = "_putchar";
+char s[] = "_putchar\n";
 while (i <= 9)
 {
 c = s[i];
 _putchar(c);
 i++;
 }
-_putchar('\n');
 return (0);
 }
