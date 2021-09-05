039dc0974ba4797326616a8fcf60c16871ca63e8
Eyumul <88399516+Eyumul@users.noreply.github.com>
2021-09-05 19:52:25 +0400

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,7 +7,8 @@
  */
 void print_number(int n)
 {
-int s0, s1, s2, s3, s4, s5, s6, s7, s8; 
+int a0, a1, a2, a3, a4, a5, a6, a7, a8, a9;
+int s0, s1, s2, s3, s4, s5, s6, s7, s8;
 print_numbers(n);
 if (s0 != 0)
 _putchar('0' + a0);
