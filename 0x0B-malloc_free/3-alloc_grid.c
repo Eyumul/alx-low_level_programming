4f6fe8ce4ad6bfe2f7f38528434b51e3c7cbc29d
Eyumul <88399516+Eyumul@users.noreply.github.com>
2021-10-15 18:06:05 +0400

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,32 +1,46 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * alloc_grid - A function that returns a pointer to a
- * 2 dimensional array of integers
- * @width: An input integer at number of columns
- * @height: An input integer at number of rows
- * Return: pointer to a 2D array, NULL on failure
+ * alloc_grid - Entry point
+ *@width: rows of matrix
+ *@height: columns of string
+ * Return: a pointer to a 2 dimensional array of integers or null
  */
 int **alloc_grid(int width, int height)
 {
-	int **array, i = 0, j = 0;
+int **matrix;
+int i;
+int j;
+int l;
+int *p;
 
-	if (width <= 0 || height <= 0)
-		return (NULL);
-
-	array = (int **)malloc(sizeof(int *) * height);
-	if (array == NULL)
-		return (NULL);
-	for (; i < height; i++)
-	{
-		array[i] = (int *)malloc(sizeof(int) * width);
-		if (array[i] == NULL)
-			return (NULL);
-	}
-
-	for (i = 0; i < height; i++)
-		for (; j < width; j++)
-			array[i][j] = 0;
-	return (array);
+if (width <= 0 || height <= 0)
+return (NULL);
+matrix = (int **)malloc(height * sizeof(int *));
+if (matrix == NULL)
+return (NULL);
+for (i = 0; i < height; i++)
+{
+*(matrix + i) = (int *)malloc(width * sizeof(int));
+if (*(matrix + i) == NULL)
+{
+for (i = 0; i < height; i++)
+{
+p = matrix[i];
+free(p);
+}
+free(matrix);
+return (NULL);
+}
+}
+for (l = 0; l < height; l++)
+{
+for (j = 0; j < width; j++)
+{
+matrix[l][j] = 0;
+}
+}
+return (matrix);
 }
