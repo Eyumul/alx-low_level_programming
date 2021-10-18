88c246787f982ee6bf1713c673d4ff315ec2ddef
Eyumul <88399516+Eyumul@users.noreply.github.com>
2021-10-18 13:27:57 +0400

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,28 +1,37 @@
-#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- *main -  prints the opcodes of its own main function.
- *@argc: integer value.
- *@argv: character value.
- *
- *Return: 0(success)
- */
+* main - prints opcodes of a given mahine
+* @argc: number of arguments.
+* @argv: argument vector
+*
+* Return: 0.
+*/
 int main(int argc, char *argv[])
 {
-int i;
+int i, size;
 
 if (argc != 2)
 {
 printf("Error\n");
 exit(1);
 }
-if (atoi(argv[1]) < 0)
+
+size = atoi(argv[1]);
+if (size < 0)
 {
 printf("Error\n");
 exit(2);
 }
-for (i = 0; i < atoi(argv[1]) - 1; i++)
-printf("%02hhx ", ((char *)main)[i]);
-printf("%02hhx\n", ((char *)main)[i]);
+
+for (i = 0; i < size; i++)
+{
+printf("%02hhx", *((char *)main + i));
+if (i < size - 1)
+printf(" ");
+else
+printf("\n");
+}
 return (0);
 }
